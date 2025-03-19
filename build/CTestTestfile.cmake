4488b33512d62919d0b27ee54cb7b46f003b7503
root <root@beilbag>
2025-03-19 14:44:46 +1100

    removed build files from repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/PERSONAL/video-edit
-# Build directory: /home/PERSONAL/video-edit/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
